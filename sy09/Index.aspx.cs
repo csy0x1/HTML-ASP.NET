dadde7e715dfa67151305722da1bd13a25939f31
csy0x1 <1439364642@qq.com>
2020-11-30 23:40:08 +0800

    Revert "sy12"
    This reverts commit cb24a5e1e143766ca4a838e8ecbb1c2ab8a03354.

--- sy09/Index.aspx.cs
+++ sy09/Index.aspx.cs
@@ -18,50 +18,6 @@ namespace csy
             {
 
             }
-            IStudent stul = new Student()
-            {
-                StuCode = "12345678",
-                StuName = "test"
-            };
-            setLog(stul.GetInfo());
-            setLog("Page_Load");
-            log.Value = Application["log"].ToString();
         }
-
-            protected void Page_Init(object sender, EventArgs e)
-            {
-            setLog("Page_Init");
-            }
-            protected void Page_InitComplete(object sender, EventArgs e)
-            {
-                setLog("Page_InitComplete");
-            }
-            protected void Page_LoadComplete(object sender, EventArgs e)
-            {
-                setLog("Page_LoadComplete");
-            }
-            protected void Page_PreInit(object sender, EventArgs e)
-            {
-                setLog("Page_PreInit");
-            }
-            protected void Page_PreLoad(object sender, EventArgs e)
-            {
-                setLog("Page_PreLoad");
-            }
-            protected void Page_PreRenderComplete(object sender, EventArgs e)
-            {
-                setLog("Page_PreRenderComplete");
-            }
-            protected void Page_SaveStateComplete(object sender, EventArgs e)
-            {
-                setLog("Page_SaveStateComplete");
-            }
-
-
-
-        private void    setLog(string value)
-            {
-                LogUtils.setLog(Application, value);
-            }
-        }
+    }
 }
--- sy09/Index.aspx.designer.cs
+++ sy09/Index.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace csy
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl SysHead;
-
-        /// <summary>
-        /// log 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputHidden log;
     }
 }
